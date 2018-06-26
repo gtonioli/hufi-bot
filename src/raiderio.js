74da3446c8d9b7a66973ced587bfb8a8b5b16e1a
Guilherme Tonioli <guilhermestonioli@gmail.com>
2018-06-26 19:20:51 -0300

    season

--- src/raiderio.js
+++ src/raiderio.js
@@ -1,9 +1,11 @@
 const axios = require("axios");
 
+const season = "season-post-legion";
+
 const RaiderIO = {
   getCharacter: async (name) => {
     let char = {};
-    await axios.get("https://raider.io/api/characters/us/azralon/" + name + "/?season=season-7.3.2&tier=21").then(async (response) => {
+    await axios.get("https://raider.io/api/characters/us/azralon/" + name + "/?season=" + season + "&tier=21").then(async (response) => {
       const runs = await RaiderIO.getCharacterDetails(response.data.characterDetails.character.id);
       char = {
         info: response.data,
@@ -20,7 +22,7 @@ const RaiderIO = {
   },
   getCharacterDetails: async (id) => {
     let details = {};
-    await axios.get("https://raider.io/api/characters/mythic-plus-scored-runs?season=season-7.3.2&role=all&mode=scored&affixes=all&date=all&characterId=" + id).then((response) => {
+    await axios.get("https://raider.io/api/characters/mythic-plus-scored-runs?season=" + season + "&role=all&mode=scored&affixes=all&date=all&characterId=" + id).then((response) => {
       details = response.data;
     }).catch(() => {
       details = {
@@ -33,7 +35,7 @@ const RaiderIO = {
   },
   getRank: async () => {
     let rank = [];
-    await axios.get("https://raider.io/api/mythic-plus/rankings/characters?region=us&realm=azralon&guild=Vem%20Pro%20Pai&season=season-7.3.2&class=all&role=all&page=0").then((response) => {
+    await axios.get("https://raider.io/api/mythic-plus/rankings/characters?region=us&realm=azralon&guild=Vem%20Pro%20Pai&season=" + season + "&class=all&role=all&page=0").then((response) => {
       rank = response.data.rankings.rankedCharacters;
     }).catch(() => {
       rank = {
